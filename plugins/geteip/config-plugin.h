529073355cb93ab8ca1440cd7061cd684ef8bda7
ntddk <ntinternal@gmail.com>
2014-09-07 15:52:24 +0900

    Replace all files with revision 261

--- plugins/geteip/config-plugin.h
+++
@@ -1,6 +0,0 @@
-#define DEFINE_INSN_BEGIN
-#define DEFINE_INSN_END
-#define DEFINE_BLOCK_BEGIN
-#define DEFINE_BLOCK_END
-#define DECAF_HOME "/home/paul/DECAF1.8"
-#define PLUGIN_PATH "/home/paul/DECAF1.8/plugins/geteip"
